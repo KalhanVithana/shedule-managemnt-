2fa96252158970572d4c27a0f5850f5a905e644b
srimali <srimaliudayangani850@gmail.com>
2022-04-16 19:00:38 +0530

    SignUp Page and changes

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'antd/dist/antd.css';
-import "./assets/SCSS/index.scss";
+import "../src/assets/SCSS/index.scss";
 import AppRoutes from './routes';
 
 function App() {
--- web/src/containers/user/forms/login.js
+++ web/src/containers/user/forms/login.js
@@ -1,11 +1,69 @@
-import React from 'react'
-
+import React from "react";
+import { Checkbox, Button, Row, Col, Input } from "antd";
+import loginImg from "../../../assets/Images/signup.png";
 export default function Login() {
   return (
     //login
     <div className="loginPageConatiner">
-<h1>Hello</h1>
+      <div>
+        <Row className="loginMiddleSection" justify="center" gutter={[0, 0]}>
+          <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+            <div
+              className="leftSectionLogin"
+              style={{
+                backgroundImage: `url(${loginImg})`,
+              }}
+            ></div>
+          </Col>
+          <Col xs={24} sm={4} md={24} lg={12} xl={12}>
+            <div className="rightSection">
+              <p className="alreadyText">
+                Already have an account?{" "}
+                <a href="#" className="loginLink">
+                  Login
+                </a>
+              </p>
+              <p className="headingCreate">Create account</p>
+              <div className="formSection">
+                <Row gutter={[30, 0]}>
+                  <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+                    <p className="label">First Name</p>
+                    <Input className="inputFeild" placeholder="First Name" />
+                  </Col>
+                  <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+                    <p className="label"> Last Name</p>
+                    <Input placeholder="Last Name" className="inputFeild" />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                    <p className="label">Email</p>
+                    <Input placeholder="Email" className="inputFeild email" />
+                  </Col>
+                </Row>
 
+                <Row gutter={[30, 0]}>
+                  <Col xs={24} sm={24} md={24} lg={24} xl={12}>
+                    <p className="label">Password</p>
+                    <Input placeholder="Password" className="inputFeild" />
+                  </Col>
+                  <Col xs={24} sm={24} md={24} lg={24} xl={12}>
+                    <p className="label">Confirm</p>
+                    <Input
+                      placeholder="Confirm Password"
+                      className="inputFeild"
+                    />
+                  </Col>
+                </Row>
+
+                <div className="acceptSection">
+                  <Button className="createBtn">Create Account</Button>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </div>
     </div>
-  )
+  );
 }
