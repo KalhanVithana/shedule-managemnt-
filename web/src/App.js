a6de8352d784248b6e4de80dc6ac0b4044bcb727
srimali <srimaliudayangani850@gmail.com>
2022-04-16 21:10:16 +0530

    update code

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+
+import 'antd/dist/antd.css';
+import "./assets/SCSS/index.scss";
 import AppRoutes from './routes';
 
 function App() {
--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -5,13 +5,13 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import Login from '../containers/user/forms/login';
+import SignUp from '../containers/user/forms/SignupPage';
 export default function AppRoutes() {
   return (
     //routes
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login />}>
+      <Route path="/" element={<SignUp />}>
         
       </Route>
     </Routes>
